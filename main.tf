017e33b546850c90a554efcbfe66c11b6aa84066
root <root@fieldhousem2.mylabserver.com>
2018-03-20 20:24:40 +0000

    updated more IaaC

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "${var.aws_region}"
+  region  = "${var.aws_region}"
   profile = "${var.aws_profile}"
 }
 
@@ -14,7 +14,7 @@ resource "aws_iam_instance_profile" "s3_access_profile" {
 
 resource "aws_iam_role_policy" "s3_access_policy" {
   name = "s3_access_policy"
-  role = "${aws_iam_role.s3_access_role.id}"  
+  role = "${aws_iam_role.s3_access_role.id}"
 
   policy = <<EOF
 {
@@ -32,7 +32,7 @@ EOF
 
 resource "aws_iam_role" "s3_access_role" {
   name = "s3_access_role"
-  
+
   assume_role_policy = <<EOF
 {
  "Version": "2012-10-17",
@@ -53,9 +53,9 @@ EOF
 #---VPC--
 
 resource "aws_vpc" "wp_vpc" {
-  cidr_block = "${var.vpc_cidr}"
+  cidr_block           = "${var.vpc_cidr}"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   tags {
     Name = "wp_vpc"
@@ -66,7 +66,7 @@ resource "aws_vpc" "wp_vpc" {
 
 resource "aws_internet_gateway" "wp_internet_gateway" {
   vpc_id = "${aws_vpc.wp_vpc.id}"
-  
+
   tags {
     Name = "wp_igw"
   }
@@ -76,24 +76,264 @@ resource "aws_internet_gateway" "wp_internet_gateway" {
 
 resource "aws_route_table" "wp_public_rt" {
   vpc_id = "${aws_vpc.wp_vpc.id}"
-  
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.wp_internet_gateway.id}"
   }
-  
+
   tags {
     Name = "wp_public"
   }
 }
 
 resource "aws_default_route_table" "wp_private_rt" {
-  default_route_table_id =  "${aws_vpc.wp_vpc.default_route_table_id}"
+  default_route_table_id = "${aws_vpc.wp_vpc.default_route_table_id}"
 
   tags {
     Name = "wp_private"
   }
 }
 
+#-------subnets-----
 
-    
+resource "aws_subnet" "wp_public1_subnet" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  # dictionary type declaration
+  cidr_block              = "${var.cidrs["public1"]}"
+  map_public_ip_on_launch = true
+
+  # retrieved first available availability zone
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_public1"
+  }
+}
+
+resource "aws_subnet" "wp_public2_subnet" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  # dictionary type declaration
+  cidr_block              = "${var.cidrs["public2"]}"
+  map_public_ip_on_launch = true
+
+  # retrieved first available availability zone
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_public2"
+  }
+}
+
+resource "aws_subnet" "wp_private1_subnet" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  # dictionary type declaration
+  cidr_block              = "${var.cidrs["private1"]}"
+  map_public_ip_on_launch = false
+
+  # retrieved next available availability zone
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_private1"
+  }
+}
+
+resource "aws_subnet" "wp_private2_subnet" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  # dictionary type declaration
+  cidr_block              = "${var.cidrs["private2"]}"
+  map_public_ip_on_launch = false
+
+  # retrieved next available availability zone
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_private2"
+  }
+}
+
+resource "aws_subnet" "wp_rds1_subnet" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  # dictionary type declaration
+  cidr_block              = "${var.cidrs["rds1"]}"
+  map_public_ip_on_launch = false
+
+  # retrieved next available availability zone
+  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_rds1"
+  }
+}
+
+resource "aws_subnet" "wp_rds2_subnet" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  # dictionary type declaration
+  cidr_block              = "${var.cidrs["rds2"]}"
+  map_public_ip_on_launch = false
+
+  # retrieved next available availability zone
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_rds2"
+  }
+}
+
+resource "aws_subnet" "wp_rds3_subnet" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  # dictionary type declaration
+  cidr_block              = "${var.cidrs["rds3"]}"
+  map_public_ip_on_launch = false
+
+  # retrieved next available availability zone
+  availability_zone = "${data.aws_availability_zones.available.names[2]}"
+
+  tags {
+    Name = "wp_rds3"
+  }
+}
+
+#rds subnet group 
+
+resource "aws_db_subnet_group" "wp_rds_subnetgroup" {
+  name = "wp_rds_subnetgroup"
+
+  subnet_ids = ["${aws_subnet.wp_rds1_subnet.id}",
+    "${aws_subnet.wp_rds2_subnet.id}",
+    "${aws_subnet.wp_rds3_subnet.id}",
+  ]
+
+  tags {
+    Name = "wp_rds_sng"
+  }
+}
+
+# Associate subnets with route tables
+
+resource "aws_route_table_association" "wp_public1_assoc" {
+  subnet_id      = "${aws_subnet.wp_public1_subnet.id}"
+  route_table_id = "${aws_route_table.wp_public_rt.id}"
+}
+
+resource "aws_route_table_association" "wp_public2_assoc" {
+  subnet_id      = "${aws_subnet.wp_public2_subnet.id}"
+  route_table_id = "${aws_route_table.wp_public_rt.id}"
+}
+
+resource "aws_route_table_association" "wp_private1_assoc" {
+  subnet_id      = "${aws_subnet.wp_private1_subnet.id}"
+  route_table_id = "${aws_default_route_table.wp_private_rt.id}"
+}
+
+resource "aws_route_table_association" "wp_private2_assoc" {
+  subnet_id      = "${aws_subnet.wp_private2_subnet.id}"
+  route_table_id = "${aws_default_route_table.wp_private_rt.id}"
+}
+
+#----Security Groups---
+
+# dev instance security group
+
+resource "aws_security_group" "wp_dev_sg" {
+  name        = "wp_dev_sg"
+  description = "Used for access to dev instance"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  # http
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"          #allow all protocol
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Public Security Group
+
+resource "aws_security_group" "wp_public_sg" {
+  name        = "wp_public_sg"
+  description = "Used for the elastic load balancer for public access"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  # http
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"          #allow all protocol
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Private Security Group
+resource "aws_security_group" "wp_private_sg" {
+  name        = "wp_private_sg"
+  description = "Used for access to instances within the private subnet"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  # http
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "-1"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"          #allow all protocol
+    cidr_blocks = ["0.0.0.0/0"] #  to allow access to the s3 endpoint
+  }
+}
+
+# RDS security group
+
+# Private Security Group
+resource "aws_security_group" "wp_rds_sg" {
+  name        = "wp_rds_sg"
+  description = "Used for accessing the RDS instances"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  # SQL access from public and private securoity groups
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+
+    security_groups = ["${aws_security_group.wp_dev_sg.id}",
+      "${aws_security_group.wp_public_sg.id}",
+      "${aws_security_group.wp_private_sg.id}",
+    ]
+  }
+}
