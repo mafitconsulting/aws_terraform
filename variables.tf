ea1abc79741b03d2b76755de5abd1155e992f624
root <root@fieldhousem2.mylabserver.com>
2018-03-24 08:04:27 +0000

    updated IaaC

--- variables.tf
+++ variables.tf
@@ -10,7 +10,23 @@ variable "cidrs" {
 variable "localip" {}
 variable "domain_name" {}
 variable "db_instance_class" {}
-variable "dbname"{}
-variable "dbuser"{}
-variable "dbpassword"{}
+variable "dbname" {}
+variable "dbuser" {}
+variable "dbpassword" {}
+variable "dev_ami" {}
+variable "public_key_path" {}
+variable "key_name" {}
+variable "dev_instance_type" {}
+variable "elb_unhealthy_threshold" {}
+variable "elb_healthy_threshold" {}
+variable "elb_timeout" {}
+variable "elb_interval" {}
 
+variable "asg_max" {}
+variable "asg_min" {}
+variable "asg_grace" {}
+variable "asg_hct" {}
+variable "lc_instance_type" {}
+variable "desired_capacity" {}
+variable "asg_cap" {}
+variable "delegation_set" {}
